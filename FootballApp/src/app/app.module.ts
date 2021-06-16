8270958bc32c606313ace20b634bff6704704ff1
Artem <ignatev.artem.nik@gmail.com>
2021-06-16 14:37:47 +0400

    Создание компонентов mainSection и table card

--- FootballApp/src/app/app.module.ts
+++ FootballApp/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { TableCardComponent } from './shared/table-card/table-card.component';
+import { SelectorMenuComponent } from './shared/selector-menu/selector-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    MainSectionComponent,
+    TableCardComponent,
+    SelectorMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ FootballApp/src/app/shared/selector-menu/selector-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-menu',
+  templateUrl: './selector-menu.component.html',
+  styleUrls: ['./selector-menu.component.css']
+})
+export class SelectorMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
