a5f84af6d4f9b0917bafc1801fb5082a04421299
Artem <ignatev.artem.nik@gmail.com>
2021-06-18 18:22:26 +0400

    Добавлены иконки и лиги

--- FootballApp/src/app/data/TestData.ts
+++ FootballApp/src/app/data/TestData.ts
@@ -4,14 +4,18 @@ export  class TestData{
 
 
   static leagues: League[] = [
-    {id: 1, title: 'Работа'},
-    {id: 2, title: 'Семья'},
-    {id: 3, title: 'Учеба'},
-    {id: 4, title: 'Отдых'},
-    {id: 5, title: 'Спорт'},
-    {id: 6, title: 'Еда'},
-    {id: 7, title: 'Финансы'},
-    {id: 8, title: 'Гаджеты'},
+    {id: 1, title: 'Английская премьер-лига', icon: '../../assets/img/icons/flags/gb-eng.svg' },
+    {id: 2, title: 'Испанская лига', icon: '../assets/img/icons/flags/es.svg'},
+    {id: 3, title: 'Итальянская серия А', icon: '../assets/img/icons/flags/it.svg'},
+    {id: 4, title: 'Германская бундеслига', icon: '../assets/img/icons/flags/de.svg'},
+    {id: 5, title: 'Французкая первая лига', icon: '../assets/img/icons/flags/fr.svg'},
+    {id: 6, title: 'Португальский первый дивизион', icon: '../assets/img/icons/flags/pt.svg'},
+    {id: 7, title: 'Российская премьер-лига', icon: '../assets/img/icons/flags/ru.svg'},
+    {id: 8, title: 'Нидерландский первый дивизион', icon: '../assets/img/icons/flags/nl.svg'},
+    {id: 9, title: 'Бельгийская первая лига', icon: '../assets/img/icons/flags/be.svg'},
+    {id: 10, title: 'Австрийская бундеслига', icon: '../assets/img/icons/flags/at.svg'},
+    {id: 11, title: 'Украинская премьер-лига', icon: '../assets/img/icons/flags/ua.svg'},
+    {id: 12, title: 'Шотландская премьер-лига', icon: '../assets/img/icons/flags/gb-sct.svg'},
 
   ];
 
